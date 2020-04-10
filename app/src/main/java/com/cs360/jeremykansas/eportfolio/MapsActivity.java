ac832dcb8b5f0f93fcfd03218b229546353ca4cb
Jeremy Kansas <ymerejsasnak@gmail.com>
2020-04-10 16:09:01 -0400

    more map work, permissions

--- app/src/main/java/com/cs360/jeremykansas/eportfolio/MapsActivity.java
+++ app/src/main/java/com/cs360/jeremykansas/eportfolio/MapsActivity.java
@@ -3,7 +3,6 @@ package com.cs360.jeremykansas.eportfolio;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Intent;
@@ -35,24 +34,26 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
-    LatLng currentLoc;
-
     // demo/hardcoded job interview is at SNHU
     LatLng jobLoc = new LatLng(43.0372, -71.4523);
+    LatLng currentLoc;
+
+    Marker jobMarker, currentMarker;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
 
+
+        // location provider lets us get current ('last') location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         // check for permission for coarse location
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -62,58 +63,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
                     PERMISSION_REQUEST_CODE);
         } else {
+            // already have permission, so get location
             getLocation();
         }
 
-
-        // get location provider and success lister so we can get actual location
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-
-
     }
 
-
+    // get location if permitted
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == PERMISSION_REQUEST_CODE &&
                 grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // permission granted, so get location
             getLocation();
-        } else {
-            finish();
         }
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // store the map once its available, then update markers and camera
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.addMarker(new MarkerOptions().position(currentLoc).title("Current Location"));
-
-        Marker jobMarker = mMap.addMarker(new MarkerOptions().position(jobLoc).title("Job Interview at SNHU"));
-        jobMarker.showInfoWindow();
-
-
-
-        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        builder.include(jobLoc).include(currentLoc);
-        LatLngBounds bounds = builder.build();
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100 * (int)this.getResources().getDisplayMetrics().density));
-
-
+        // update map markers and camera
+        updateMarkers();
+        updateCamera();
     }
-
 
     // add menu buttons to top bar - launch maps app for directions
     @Override
@@ -128,19 +103,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.get_directions) {
 
+            // build query string for map url (opens in app or web if app unavailable)
+            // origin omitted because it defaults to current location
             String destination = jobLoc.latitude + "," + jobLoc.longitude;
             String url = "https://www.google.com/maps/dir/?api=1&destination=" + destination;
 
+            // and run it
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse(url));
             startActivity(intent);
-
         }
         return super.onOptionsItemSelected(item);
-
     }
 
-    public void getLocation() {
+    // get user's last location and, on success, store it in currentLoc
+    private void getLocation() {
         fusedLocationProviderClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
@@ -148,11 +125,39 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
                             currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
-
-                            mapFragment.getMapAsync(MapsActivity.this);
+                            // update markers and camera
+                            updateMarkers();
+                            updateCamera();
                         }
                     }
                 });
     }
 
+    // I don't think this is the best way to go about this, but it works for now...
+    // want to be able to create markers and update camera based on not knowing whether other
+    // methods will succeed or when they will complete
+    private void updateMarkers() {
+        // if needed, add marker for job location, show its info window
+        if (jobMarker == null) {
+            jobMarker = mMap.addMarker(new MarkerOptions().position(jobLoc).title("Job Interview at SNHU"));
+            jobMarker.showInfoWindow();
+        }
+        // if needed, add marker for current location
+        if (currentLoc != null && currentMarker == null) {
+            currentMarker = mMap.addMarker(new MarkerOptions().position(currentLoc).title("Current Location"));
+        }
+
+    }
+
+    // setup camera using lat/lng of both locations, or just jobLoc if no currentLoc
+    private void updateCamera() {
+        if (currentLoc != null){
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(jobLoc).include(currentLoc);
+            LatLngBounds bounds = builder.build();                       // 200dp padding, somewhat arbitrary...
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200 * (int) this.getResources().getDisplayMetrics().density));
+        } else {                                                    // 13 is between city and street level zoom
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jobLoc, 13));
+        }
+    }
 }
